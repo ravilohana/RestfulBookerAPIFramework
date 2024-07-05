b9477a774d69270eeb934ee6bf7fe5439437004e
ravilohana <ravi.lohana1987@gmail.com>
2024-07-05 20:57:26 +0530

    add parallel supports for create and update booking api

--- src/test/java/CreateBookingApiTests.java
+++ src/test/java/CreateBookingApiTests.java
@@ -1,4 +1,5 @@
 import apis.CreateBookingApi;
+import apis.DeleteBookingApi;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -15,25 +16,32 @@ import static org.hamcrest.Matchers.*;
 public class CreateBookingApiTests {
 
 
-    private CreateBookingApi createBookingApi;
 
 
-    @BeforeClass
-    public void initApi() {
-        createBookingApi = new CreateBookingApi();
 
-    }
 
 
     @Test(description = "Create a new booking record (Using Map) and validate status code 200",dataProvider = "bookingDataWithForLoop")
     public void createAndValidateStatusCode(String firstname, String lastname, Long totalPrice, Boolean depositPaid, String additionalNeeds,
                                             String checkInDate, String checkOutDate) {
 
+        var createBookingApi = new CreateBookingApi();
+        var deleteBookingApi = new DeleteBookingApi();
         var createBookingPayload = ApiRequestHelper.createBookingPayload(firstname, lastname, Math.toIntExact(totalPrice),
                 depositPaid, checkInDate, checkOutDate, additionalNeeds);
-        var createBookingApiResponse = this.createBookingApi.createNewBooking(createBookingPayload)
+        var createBookingApiResponse = createBookingApi.createNewBooking(createBookingPayload)
                 .then().assertThat().statusCode(200)
                 .and().body("bookingid", is(not(equalTo(0))));
+
+        int bookingID = createBookingApiResponse.extract().jsonPath().getInt("bookingid");
+
+        // delete booking api
+
+        String username = System.getenv("RESTFUL_BOOKER_USERNAME");
+        String password = System.getenv("RESTFUL_BOOKER_PASSWORD");
+
+        var deleteBookingApiByID = deleteBookingApi.deleteBookingApiById(bookingID, username, password)
+                                                   .then().assertThat().statusCode(201);
 
     }
 
@@ -41,9 +49,10 @@ public class CreateBookingApiTests {
     public void createAndValidateStatusCodeViaPOJO(String firstname, String lastname, Long totalPrice, Boolean depositPaid, String additionalNeeds,
                                                    String checkInDate, String checkOutDate) {
 
+        CreateBookingApi createBookingApi = new CreateBookingApi();
         var createBookingPayload = ApiRequestHelper.createBookingRequestPOJO(firstname, lastname, Math.toIntExact(totalPrice),
                 depositPaid, checkInDate, checkOutDate, additionalNeeds);
-        var createBookingApiResponse = this.createBookingApi.createNewBooking(createBookingPayload)
+        var createBookingApiResponse = createBookingApi.createNewBooking(createBookingPayload)
                 .then().assertThat().statusCode(200)
                 .and().body("bookingid", is(not(equalTo(0))));
 
@@ -52,7 +61,7 @@ public class CreateBookingApiTests {
 
     // Data provider
     // With for loop
-    @DataProvider(name = "bookingDataWithForLoop")
+    @DataProvider(name = "bookingDataWithForLoop" ,parallel = true)
     public Object[][] bookingDataProviderWithLoop(){
         var faker =  TestDataHelper.getFaker();
         var name = faker.name();
@@ -60,7 +69,7 @@ public class CreateBookingApiTests {
         var numberOfPlusDays = TestDataHelper.getRandomInt(2);
 
         List<Object[]> list = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             Object[] objects = new Object[]{name.firstName(), name.lastName(), faker.number().randomNumber(3, true)
                     , faker.bool().bool(), faker.food().dish(), TestDataHelper.getFutureDate(numberOfPlusDays, dateFormatter)
                     , TestDataHelper.getFutureDate(numberOfPlusDays+4, dateFormatter)};
--- src/test/java/UpdateBookingApiTest.java
+++ src/test/java/UpdateBookingApiTest.java
@@ -1,4 +1,5 @@
 import apis.CreateBookingApi;
+import apis.DeleteBookingApi;
 import apis.GetBookingApi;
 import apis.UpdateBookingApi;
 import com.github.javafaker.Faker;
@@ -16,28 +17,24 @@ import static org.hamcrest.Matchers.*;
 public class UpdateBookingApiTest {
 
 
-    private UpdateBookingApi updateBookingApi;
-    private CreateBookingApi createBookingApi;
-    private GetBookingApi getBookingApi;
+
     private final Faker faker = TestDataHelper.getFaker();
 
-    @BeforeClass
 
-    public void initApi() {
-        updateBookingApi = new UpdateBookingApi();
-        createBookingApi = new CreateBookingApi();
-        getBookingApi = new GetBookingApi();
-
-    }
 
     @Test(description = "Create new booking and update same booking and validate status code", dataProvider = "bookingDataWithStream")
     public void createNewBookingAndUpdate(String firstname, String lastname, Long totalPrice, Boolean depositPaid, String additionalNeeds,
                                           String checkInDate, String checkOutDate) {
 
+
+        CreateBookingApi createBookingApi = new CreateBookingApi();
+        UpdateBookingApi updateBookingApi = new UpdateBookingApi();
+        DeleteBookingApi deleteBookingApi = new DeleteBookingApi();
+
         // create new booking
         var createNewBookingRequestPayload = ApiRequestHelper.createBookingPayload(firstname, lastname, Math.toIntExact(totalPrice),
                 depositPaid, checkInDate, checkOutDate, additionalNeeds);
-        var createNewBookingResponse = this.createBookingApi.createNewBooking(createNewBookingRequestPayload)
+        var createNewBookingResponse = createBookingApi.createNewBooking(createNewBookingRequestPayload)
                                                             .then().assertThat().statusCode(200)
                                                             .and().body("bookingid", is(not(equalTo(0))));
 
@@ -61,21 +58,29 @@ public class UpdateBookingApiTest {
                 this.faker.food().dish().concat(" Update needs"));
 
 
-        var updateBookingResponse = this.updateBookingApi.updateBooking(updateBookingRequestPayload, bookingID, username, password)
+        var updateBookingResponse = updateBookingApi.updateBooking(updateBookingRequestPayload, bookingID, username, password)
                                                          .then().assertThat().statusCode(200);
+
+
+        // delete booking api
+
+        var deleteBookingApiByID = deleteBookingApi.deleteBookingApiById(bookingID, username, password)
+                                                   .then().assertThat().statusCode(201);
     }
 
 
     @Test(description = "Update existing booking and validate status code")
     public void updateExistingBooking() {
 
+        UpdateBookingApi updateBookingApi = new UpdateBookingApi();
+        GetBookingApi getBookingApi = new GetBookingApi();
 
-        var getBookingIdsResponse = this.getBookingApi.getAllBookingIds()
+        var getBookingIdsResponse = getBookingApi.getAllBookingIds()
                                                       .then().assertThat().statusCode(200);
 
         int bookingID = getBookingIdsResponse.extract().jsonPath().getInt("[0].bookingid");
 
-        var getBookingByIdResponse = this.getBookingApi.getBookingById(bookingID);
+        var getBookingByIdResponse = getBookingApi.getBookingById(bookingID);
 
 
         System.out.println("Existing Booking Details: ");
@@ -95,7 +100,7 @@ public class UpdateBookingApiTest {
                 false, "2024-10-10", "2024-11-11", "Nothing Else");
 
 
-        var updateBookingResponse = this.updateBookingApi.updateBooking(updateBookingRequestPayload, bookingID, username, password)
+        var updateBookingResponse = updateBookingApi.updateBooking(updateBookingRequestPayload, bookingID, username, password)
                                                          .then().assertThat().statusCode(200)
                                                          .and().assertThat().body("firstname", is(equalTo("Newman")))
                                                          .and().assertThat().body("lastname", is(equalTo("Hokes")))
@@ -105,13 +110,13 @@ public class UpdateBookingApiTest {
 
     // Data provider
     // With Stream
-    @DataProvider(name = "bookingDataWithStream")
+    @DataProvider(name = "bookingDataWithStream",parallel = true)
     public Object[][] bookingDataProviderWithStream() {
         var faker = this.faker;
         var name = faker.name();
         var dateFormatter = DateTimeFormatter.ISO_DATE;
 
-        return IntStream.range(0, 3)
+        return IntStream.range(0, 5)
                         .mapToObj(i -> {
 
                             var numberOfPlusDays = TestDataHelper.getRandomInt(2);
